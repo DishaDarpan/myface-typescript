d1c691726a57ec4044295d2eca0b6d673b0eb346
Disha Pandit <Disha.Pandit@softwire.com>
2022-08-25 20:20:27 +0100

    Started with Mobile Menu

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Add your JavaScript here.
+  const button=document.getElementById("trybutton");
+  button.addEventListener("click",function(){
+    const postImages=document.getElementsByClassName("postImage");
+    for(let i=0;i<postImages.length;i++){
+    postImages[i].style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    }
+  })
 });
+
+// document.getElementsByClassName()
+// document.getElementsByTag()
+
+// {style: (buttonstyle)...}
+
+// [{style: divstyle}, {style: divstyle}]
